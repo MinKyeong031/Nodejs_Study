1c6aef3383f34dec23bbd5e9b08f14ee88de6226
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-04-02 15:10:28 +0900

    210402

--- NodeExample/ch04_test11.js
+++ NodeExample/ch04_test11.js
@@ -6,7 +6,7 @@ var outfile = fs.createWriteStream('./output2.txt', { flags: 'w' });
 infile.on('data', function(data) {
     console.log('3104 김민경 - 읽어들인 데이터', data);
     console.log(data);
-    console.log('첫 번째 버퍼의 문자열 : %s', data.toString());
+    console.log('첫 번째 버퍼의 문자열 : %s\n', data.toString());
     outfile.write(data);
 });
 
